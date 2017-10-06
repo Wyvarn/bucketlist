656ecd5407b1951fd24636bce9e972c865caf5ae
Brian Lusina <lusinabrian@gmail.com>
2017-10-06 08:44:13 +0300

    URL creation
    creating api view for posting data
    update to including the view on the api application to the project

---
+++ bucketListApi/api/urls.py
@@ -0,0 +1,13 @@
+"""
+The format_suffix_pattern allows us to specify the data format (raw json or even html) when we use the URLs. It appends the format to be used to every URL in the pattern.
+"""
+
+from django.conf.urls import url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from .views import CreateView
+
+urlpatterns = {
+    url("^bucketlists/$", CreateView.as_view(), name="create")
+}
+
+urlpatterns = format_suffix_patterns(urlpatterns)
--- bucketListApi/api/views.py
+++ bucketListApi/api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from rest_framework import generics
+from .serializers import BucketListSerializer
+from .models import BucketList
 
-# Create your views here.
+
+class CreateView(generics.ListCreateAPIView):
+    """
+    This defines the create behaviour for our api view
+    The ListCreateAPIView is a generic view which provides GET (list all) and POST method handlers
+    """
+    queryset = BucketList.objects.all()
+    serializer_class = BucketListSerializer
+
+    def perform_create(self, serializer):
+        """
+        Save the post data when create a new bucket list item
+        :param serializer serializer to map model object to JSON
+        """
+        serializer.save()
