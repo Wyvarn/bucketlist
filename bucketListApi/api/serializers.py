f918b7166a7b2a732639424967a4302c6290d85b
Brian Lusina <lusinabrian@gmail.com>
2017-10-06 08:24:33 +0300

    Create Serializer
    Serializer will be used to map the model instance to JSON format
    This will allow for easy api responses and testing and greatly reduces the boilerplate code that needs to be written

---
+++ bucketListApi/api/serializers.py
@@ -0,0 +1,14 @@
+from rest_framework.serializers import ModelSerializer
+from .models import BucketList
+
+
+class BucketListSerializer(ModelSerializer):
+    """
+    Serializer to map the Model instance to JSON
+    """
+
+    class Meta:
+        model = BucketList
+        fields = ("id", "name", "date_created", "date_modified")
+        read_only_fields = ("date_created", "date_modified")
+    
